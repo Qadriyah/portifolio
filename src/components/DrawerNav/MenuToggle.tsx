2a0a3d7799752f10cd174e1eee18328ee7fc8ce3
Baker Sekitoleko <b.alzawad@gmail.com>
2024-04-21 13:07:17 +0300

    Feat Adds a small device menu

--- src/components/DrawerNav/MenuToggle.tsx
+++ src/components/DrawerNav/MenuToggle.tsx
@@ -1,45 +1,14 @@
 import React, { FunctionComponent } from "react";
-import { motion } from "framer-motion";
-
-const Path = (props: any) => (
-  <motion.path
-    fill="#fff"
-    strokeWidth="3"
-    stroke="hsl(0, 0%, 100%)"
-    strokeLinecap="round"
-    {...props}
-  />
-);
 
 type IProps = {
   toggle: () => void;
+  isOpen: boolean;
 };
 
-const MenuToggle: FunctionComponent<IProps> = ({ toggle }) => {
+const MenuToggle: FunctionComponent<IProps> = ({ isOpen, toggle }) => {
   return (
-    <button onClick={toggle}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
-        <Path
-          variants={{
-            closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" },
-          }}
-        />
-        <Path
-          d="M 2 9.423 L 20 9.423"
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 },
-          }}
-          transition={{ duration: 0.1 }}
-        />
-        <Path
-          variants={{
-            closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" },
-          }}
-        />
-      </svg>
+    <button onClick={toggle} className="block md:hidden">
+      <div className="toggleIcon" data-isOn={isOpen} />
     </button>
   );
 };
