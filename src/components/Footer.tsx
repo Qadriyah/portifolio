9906d1e2fd849f174452c65f545255e7d6ff397d
Baker Sekitoleko <b.alzawad@gmail.com>
2024-04-21 08:20:59 +0300

    Feat: Code refactoring

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-5 bg-[#12181f]">
+    <footer className="p-3 bg-[#12181f]">
       <div>
         <p className="text-sm text-center">
-          Designed and built by Baker Sekitoleko &copy;{" "}
-          {`${new Date().getFullYear()}`}
+          &copy; {`${new Date().getFullYear()} Baker Sekitoleko`}
         </p>
       </div>
     </footer>
